3309e302a261f9f7c672a8278fb39b42c5b7ff2d
NP <npicciano79@gmail.com>
2025-07-08 06:50:25 -0400

    'update'

--- router_monitor_main.py
+++ router_monitor_main.py
@@ -1,1 +1,11 @@
 #router_monitor_main driver program
+from pynetgear import Netgear
+import configparser
+
+config = configparser.ConfigParser()
+
+config.read ('router_config.ini')
+
+print(config.get('ServerPass'))
+
+    #netgear = Netgear(password='pass')
